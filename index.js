16fcd8a4b03204be97c27713046d9b932213d813
plexx2g@icloud.com <plexx2g@icloud.com>
2018-08-13 19:07:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ function logWhisper(string) {
   return string.toLowerCase()
 }
 function sayHiToGrandma(string) {
-  console.log('hello')
-  return 'I can\'t hear you!'
-  
-  console.log('string')
-  return "YES INDEED!"
-  
-  console.log('I love you, Grandma.')
-  return 'I love you, too.'
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!"
+  } 
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  if (string === 'I love you, Grandma.') {
+    return "I love you, too"
+  }
 }
