1ee71ce155973752d36b45e3749931bb29496b0a
plexx2g@icloud.com <plexx2g@icloud.com>
2018-08-13 18:44:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,3 @@ function sayHiToGrandma(string) {
   console.log('hello')
   return 'I can\'t hear you!'
 }
-function sayHiToGrandma(string) {
-  console.log('HELLO')
-  return 'YES INDEED!'
-}
-function sayHiToGrandma(string) {
-  console.log('I love you, Grandma')
-  return 'I love you too'
-}
